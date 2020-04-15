1f8bf9e8d891e4b14b8aa522a3d3032595b17a08
RaresTudorache <rares@RaresLaptop.localdomain>
2020-04-15 15:54:21 +0300

    createMeal function added

--- index.js
+++ index.js
@@ -2,5 +2,27 @@ const get_meal_btn = document.getElementById("get-meal");
 const meal_container= document.getElementById("meal");
 
 get_meal_btn.addEventListener('click', () =>{
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(res => {
+        createMeal(res.meals[0]);
+    })
+    .catch(e => {
+        console.warn(e);
+    });
 });
+
+const createMeal = meal => {
+    const ingredients = [];
+
+    //get all the ingredients from the object
+    for(let i =1;i<=20;i++){
+        if(meal[`strIngredient${i}`]){
+            ingredients.push(
+                `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+            );
+        }else {
+            break;}
+    }
+};
+
